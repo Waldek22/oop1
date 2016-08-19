668444112a916c30b5d12e621df6d11114acf4f6
Waldek22 <michalwalus1@tlen.pl>
2016-08-19 19:22:25 +0200

    rework code

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var iPhone6  = new Phone('Apple', 'Iphone 6', 'biały');
-var galaxyS6  = new Phone('Samsung', 'Galaxy s6', 'czarny');
-var nexus6  = new Phone('Motorola', 'Nexus 6', 'srebrny');
-var p8Lite  = new Phone('Huawei', 'P8 Lite', 'złoty');
-var htc820  = new Phone('HTC', '820', 'niebieski');
+var iPhone6  = new Phone('Apple', 'Iphone 6', 'biały'),
+    galaxyS6  = new Phone('Samsung', 'Galaxy s6', 'czarny'),
+    nexus6  = new Phone('Motorola', 'Nexus 6', 'srebrny'),
+    p8Lite  = new Phone('Huawei', 'P8 Lite', 'złoty'),
+    htc820  = new Phone('HTC', '820', 'niebieski');
 function Phone(brand, model, color){
 	this.brand = brand,
 	this.model = model,
 	this.color = color
 }
 Phone.prototype.showPhoneInfo = function() {
-	console.log('Marka telefonu to ' + this.brand + ', jego model to ' + this.model + ', a jego kolor to ' + this.color + '.');
 	var info = '<h3>Marka telefonu to ' + this.brand + ', jego model to ' + this.model + ', a jego kolor to ' + this.color + '.</h3>';
 	$('body').append(info);
 }
